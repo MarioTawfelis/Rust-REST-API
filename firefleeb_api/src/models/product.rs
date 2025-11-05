c5135384d37f52895b2be4c5d5a60946468e1a04
mariotawfelis <mario.tawfelis@outlook.com>
2025-11-05 12:47:52 +0100

    Add products model

--- firefleeb_api/src/models/product.rs
+++ firefleeb_api/src/models/product.rs
@@ -1,15 +1,47 @@
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+use bigdecimal::BigDecimal;
+
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
 use crate::schema::products;
 
-#[derive(Debug, Queryable, Identifiable, Serialize, Deserialize)]
+#[derive(Debug, Queryable, Selectable, Identifiable, Serialize, Deserialize)]
 #[diesel(table_name = products)]
 pub struct Product {
-    pub id: uuid::Uuid,
+    pub id: Uuid,
     pub product_name: String,
     pub product_description: Option<String>,
-    pub price: bigdecimal::BigDecimal,
+    pub price: BigDecimal,
     pub stock: i32,
-    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = products)]
+pub struct NewProduct {
+    pub product_name: String,
+    pub product_description: Option<String>,
+    pub price: BigDecimal,
+    pub stock: i32,
+}
+
+#[derive(Debug, AsChangeset, Serialize, Deserialize)]
+#[diesel(table_name = products)]
+pub struct UpdateProduct {
+    pub product_name: Option<String>,
+    pub product_description: Option<String>,
+    pub price: Option<BigDecimal>,
+    pub stock: Option<i32>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ProductResponse {
+    pub id: Uuid,
+    pub product_name: String,
+    pub product_description: Option<String>,
+    pub price: BigDecimal,
+    pub stock: i32,
+    pub created_at: Option<DateTime<Utc>>,
 }
